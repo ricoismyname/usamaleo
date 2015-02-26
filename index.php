0d116d3358bcb725f2c6c4e46774f05cd6c5b39a
ricoismyname <e.orfert@gmail.com>
2015-02-27 00:15:43 +0100

    jkhgf

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $strApiKey = "4d24f8a42cee7863bde3ce14d3bea1ed";
 
 
 
-$api = "https://api.themoviedb.org/3/movie/550?api_key=" . $strApiKey;
+$api = "https://api.themoviedb.org/3/search/movie?query=fury&api_key=" . $strApiKey;
 $result = file_get_contents( $api);
 
 print_r( $result);
