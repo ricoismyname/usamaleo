be0381561ad908e9527efd05b6b12bb86c061d67
ricoismyname <e.orfert@gmail.com>
2015-02-27 00:09:56 +0100

    buzf

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
-echo "hulda is on the world 123 456";
+
+$strApiKey = "4d24f8a42cee7863bde3ce14d3bea1ed";
+
+
+
+$api = "https://api.themoviedb.org/3/movie/550?api_key=" . $strApiKey;
+$result = file_get_contents( $api);
+
+print_r( $result);
+
+
+
