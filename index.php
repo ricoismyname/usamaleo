3b774bdbe96d062688fa807198cdb69ad75c3213
ricoismyname <e.orfert@gmail.com>
2015-02-27 00:38:49 +0100

    kjhgfd

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
+require_once 'lib/tmdb-api.php';
 
 $strApiKey = "4d24f8a42cee7863bde3ce14d3bea1ed";
 
+$objApi = new TMDB( $strApiKey);
 
+$objMovie = $objApi->getLatestMovie();
 
-$api = "https://api.themoviedb.org/3/search/movie?query=fury&api_key=" . $strApiKey;
-$result = file_get_contents( $api);
-
-print_r( $result);
-
-
+print_r( $objMovie);
 
