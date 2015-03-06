70ea63324c4741ddd3cb7dc6da765ab437ea12e4
Enrico Orfert <e.orfert@cytainment.de>
2015-03-06 16:32:55 +0100

    trakt test

---
+++ test_trakt.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'lib/trakt-api.php';
+
+$objApi = Trakt::initApiCalls();
+
+// $objApi = new Trakt( "55d2fd4fe152435eb26b83c2a58d8192271fa3bcde748daeed7e6ef0af16ba6e");
+// var_dump( $objApi->searchMovies("28 Days Later"));
